86efcbca69819b00224ea7be81f6af1819fe058f
Faisal Aziz <faziz6006@gmail.com>
2020-09-05 08:57:18 -0500

    endpoints pt 2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const actionsRouter = require("./routes/actions-Router");
-
+const projectRouter = require("./routes/project-Router");
 const server = express();
 server.use(express.json());
 
 
 server.use("/api/Action", actionsRouter);
-
+server.use("/api/projects", projectRouter);
 module.exports = server;
